9b57fd7dfa1c65b5cb0542f989729aeae0f70d73
Daulet Zhumadilov <daulet140@gmail.com>
2016-02-23 17:21:23 +0600

    d

---
+++ OpenCVУ/work.cpp
@@ -0,0 +1,78 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <math.h>
+//opencv lib
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+
+CvCapture* capture = NULL;
+IplImage* image = 0;
+
+int main(int argc, char* argv[])
+{	//CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY);// 
+	CvCapture* capture = cvCreateFileCapture("vid.mp4");
+	cvNamedWindow("capture", 0);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);
+	
+	CvFont font;
+	cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 0.4, 0.4, 0, 1, 8);
+	
+	while (true)
+	{
+		image = cvQueryFrame(capture);
+		cv::Mat src = cv::cvarrToMat(image);
+		cv::Mat gray;
+		cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+		cv::CascadeClassifier cascadeSymbol;
+		cv::CascadeClassifier cascadeSymbol1;
+	    
+		//car
+		bool cascadeSymbolLoad = cascadeSymbol.load("car.xml");
+		std::vector<cv::Rect> symbols;
+		cascadeSymbol.detectMultiScale(gray, symbols);
+		for (auto& c : symbols)
+		{
+			cv::Point symbolBegin = cv::Point(c.x, c.y);
+			cv::Point symbolEnd = cv::Point(c.x + c.width, c.y + c.height);
+			rectangle(src, symbolBegin, symbolEnd, cv::Scalar(0, 0, 255), 1);
+			cvPutText(image, "car", symbolBegin, &font, cvScalar(255, 255, 255));
+		}
+	//person
+		bool cascadeSymbol1Load = cascadeSymbol1.load("person.xml");
+		std::vector<cv::Rect> symbols1;
+		cascadeSymbol1.detectMultiScale(gray, symbols1);
+		for (auto& p : symbols1)
+		{
+			cv::Point symbolBegin = cv::Point(p.x-5, p.y-5);
+			cv::Point symbolEnd = cv::Point(p.x + p.width, p.y + p.height);
+			rectangle(src, symbolBegin, symbolEnd, cv::Scalar(0, 255, 0), 1);
+			cvPutText(image, "person", symbolBegin, &font, cvScalar(255, 255, 255));
+		}
+		
+		IplImage* img = new IplImage(src);
+		cvShowImage("capture", img);
+		char x = cvWaitKey(20);
+		
+		if (x == 27) {
+			break;
+		}
+		/*#include <opencv2/opencv.hpp>
+		int counter = 0;
+		char filename[512];
+		else if (x == 13) {
+		sprintf(filename, "Image%d.jpg", 0);
+		printf("[i] capture... %s\n", filename);
+		cvSaveImage(filename, image);
+		counter++;
+		}*/
+	}
+
+
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("capture");
+	return 0;
+}
